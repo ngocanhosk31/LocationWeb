133832eb7ec2e2931235d0119be9f16d80534955
ngocanhosk31 <anhptnfx12840@funix.edu.vn>
2022-10-14 12:22:42 +0900

    create view use thymeleaf

--- src/main/java/com/locationWeb/controller/LocationController.java
+++ src/main/java/com/locationWeb/controller/LocationController.java
@@ -1,12 +1,63 @@
 package com.locationWeb.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.locationWeb.entities.Location;
+import com.locationWeb.service.LocationService;
 
 @Controller
 public class LocationController {
+	@Autowired
+	LocationService service;
+
 	@RequestMapping("/showCreate")
 	public String showCreate() {
 		return "createLocation";
 	}
+
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+		Location savedLocation = service.saveLocation(location);
+		String msg = "Location saved with id: " + savedLocation.getId();
+		modelMap.addAttribute("msg", msg);
+		return "createLocation";
+		
+	}
+	@RequestMapping("/displayLocations")
+	public String displayLocations(ModelMap modelMap) {
+		List<Location> allLocation = service.getAllLocation();
+		modelMap.addAttribute("locations", allLocation);
+		return"displayLocations";
+	}
+	@RequestMapping("/deleteLocation")
+	public String deleteLocation(@RequestParam int id, ModelMap modelMap) {
+//		Location locationById = service.getLocationById(id);
+		Location locationById = new Location();
+		locationById.setId(id);
+		service.deleteLocation(locationById);
+		List<Location> allLocation = service.getAllLocation();
+		modelMap.addAttribute("locations", allLocation);
+		return"displayLocations";
+	}
+	@RequestMapping("/showUpdate")
+	public String showUpdate(@RequestParam int id, ModelMap modelMap) {
+		Location locationById = service.getLocationById(id);
+		modelMap.addAttribute("location", locationById);
+		return "editLocation";
+	}
+	@RequestMapping("/updateLoc")
+	public String updateLocation(@ModelAttribute Location location, ModelMap modelMap) {
+		service.updateLocation(location);
+		List<Location> allLocation = service.getAllLocation();
+		modelMap.addAttribute("locations", allLocation);
+		return "displayLocations";
+	}
+
 }
--- src/main/java/com/locationWeb/entities/Location.java
+++ src/main/java/com/locationWeb/entities/Location.java
@@ -1,30 +1,34 @@
 package com.locationWeb.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name="location")
+@Table(name = "location_table")
 public class Location {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-private int id;
-private String code;
-private String name;
-private String type;
-public Location(String code, String name, String type) {
-	super();
-	this.code = code;
-	this.name = name;
-	this.type = type;
-}
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column
+	private String code;
+	@Column
+	private String name;
+	@Column
+	private String type;
+
+	public Location(String code, String name, String type) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.type = type;
+	}
 
 }
